1d21faa4310c415e65d1ebb485f332a637343384
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-05 20:08:29 +0200

    Implementing notice markup

--- src/Notices/Formatters/WordPressSticky.php
+++ src/Notices/Formatters/WordPressSticky.php
@@ -14,8 +14,32 @@ namespace Pan\Notices\Formatters;
 
 use Pan\Notices\WP_Notice;
 
-class WordPressSticky extends Formatter{
+/**
+ * Class WordPressSticky
+ *
+ * @package Pan\Notices\Formatters
+ * @author  Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * @since   TODO ${VERSION}
+ */
+class WordPressSticky extends Formatter {
+	/**
+	 * @param WP_Notice $notice
+	 *
+	 * @return string
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
 	public function formatOutput( WP_Notice $notice ) {
-		// TODO Implement
+		$out = "
+		<div style=\"position: relative;\" class=\"{$notice->getType()}\">
+			<h4 style=\"margin-top: 4px; margin-bottom: 0px;\">{$notice->getTitle()}</h4>
+			<p>
+				{$notice->getContent()}
+				<a href=\"{{ dismissLink }}\" style=\"font-size: 150%; position: absolute; right: 5px; top: -5px;\">×</a>
+			</p>
+		</div>
+		";
+
+		return $out;
 	}
 }
